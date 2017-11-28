a66214f47d696452b776acc4108a71680b6ba85f
schatell <sebastien.chatelle.blanchard@gmail.com>
2017-11-28 15:48:03 -0500

    css

--- public/javascript.js
+++ public/javascript.js
@@ -1,7 +1,9 @@
 // Center text on nav link //
 $(document).ready(function(){
+    // target element
     $('.nav-link').each(function(){
         var height = $(this).height();
+        // Affect lineheight same as height of element
         $(this).css('lineHeight', (height) + "px");
     });
 });
